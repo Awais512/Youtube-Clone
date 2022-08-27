69e8e45e7dc6da1c7f6a7ba447f2135f27d3072b
awais512 <raza.awais25@gmail.com>
2022-08-27 18:39:56 +0500

    Showing Video Card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -33,7 +33,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright 2022 Awais Chaudhary
+          Copyright 2022 Awais
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
